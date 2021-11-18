64aff4a5ed1804d53762c9c01d7c36e34480563c
dongboxiansheng <88574640+dongboxiansheng@users.noreply.github.com>
2021-11-18 15:31:44 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <h1>Todolist</h1>
-  <h3>添加新的todo</h3>
-  <div class="form-group">
-    <input type="text" placeholder="添加新的todo" id="add" class="form-control" v-model="state.value" @keydown.enter="add">
-    <el-button type="primary" @click="add">添加todo</el-button>
+  <el-form class="demo-ruleForm" >
+    <div class="header">
+    <el-input v-model="input" placeholder="添加新的todo"  id="add"  @keydown.enter="add" class="input" />
+    <!-- <input type="text" placeholder="添加新的todo" id="add" class="form-control" v-model="state.value" @keydown.enter="add"> -->
+    <el-button type="primary" @click="add" class="button">添加todo</el-button>
   </div>
+   <br>
+   <br>
   <Child :state="state" @remove="remove" @change="change" :finished="finished" :count="count" :donecount="donecount"></Child>
+  </el-form>
 </template>
 
-<script   setup >
+<script   setup lang="ts">
 import Child from './components/Child.vue'
 import { reactive,ref,computed} from "vue";
+const input =ref([])
 const count = ref(3)
 const donecount = ref(0)
 const remove= (index) =>{
@@ -18,13 +23,14 @@ const remove= (index) =>{
      state.lists.splice(index,1)
    }
   const change = ()=>{
-    donecount.value++;
     count.value--;
+    donecount.value++;
+    console.log(finished.value);
   }
   const add = ()=>{
   count.value++;
    state.lists.push({
-      name:state.value,
+      name:input.value,
        checked:false,
        isEdit:false,
    })
@@ -34,7 +40,7 @@ const state =reactive({
   value:'',
   lists:[
     {
-        name:'1',
+        name :'1',
         checked:false,
         isEdit:false
       },
@@ -53,5 +59,19 @@ const state =reactive({
 const finished = computed(()=>{
   return state.lists.filter((item)=>item.checked==true)
 })
+
 </script>
+
+<style lang="scss">
+.header{
+  position: relative;
+  .input{
+    position: absolute;
+  }
+  .button{
+    right: 0px;
+    position: absolute;
+  }
+}
+</style>
 
